7f4c4bd686de6469352ae9eacb396e0a7df0f81a
Technology Universe <technologyuniverse364515@gmail.com>
2026-05-02 14:04:25 +0530

    update styling for github pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
   resolve: {
